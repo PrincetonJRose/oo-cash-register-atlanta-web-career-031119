3d71066e6174db08ff7ae17dd6a67d9272df7643
Princeton Rose <decoyzpr@gmail.com>
2019-03-13 20:15:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,10 +12,10 @@ class CashRegister
     @total
   end
 
-  def add_item(title, price, quantity = 1)
+  def add_item(title, price, *quantity = 1)
     total += price * quantity
   end
 
-  
+
 
 end
